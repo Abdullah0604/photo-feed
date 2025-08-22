2438b794cf3c327c701d1e44d5b2421068a0e083
greenFruits <mdabdullah6548ctg@gmail.com>
2025-08-22 19:14:30 +0600

    feat: added intercepting in photo details page

---
+++ components/Modal.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ children }) {
+  const modalRef = useRef(null);
+  const router = useRouter();
+  const onHide = () => {
+    router.back();
+  };
+  useEffect(() => {
+    if (!modalRef.current?.open) {
+      modalRef.current.showModal();
+    }
+  }, []);
+
+  return createPortal(
+    <dialog
+      ref={modalRef}
+      onClose={onHide}
+      className="shadow-teal-200 shadow-md border border-teal-600 flex flex-col p-2 rounded-md "
+    >
+      <span onClick={onHide} className="flex justify-end cursor-pointer">
+        <Image src="/xmark.svg" alt="close" width={30} height={30} />
+      </span>
+      {children}
+    </dialog>,
+    document.getElementById("modal-root-content")
+  );
+}
+
+export default Modal;
